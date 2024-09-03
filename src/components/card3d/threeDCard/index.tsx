2deb663a53be52522ae190e366bfc9ec469989af
Brian Gelvez <briannn97@gmail.com>
2024-09-03 14:53:20 -0300

    styles: Arreglo de stilo de imagen en las cardss

--- src/components/card3d/threeDCard/index.tsx
+++ src/components/card3d/threeDCard/index.tsx
@@ -27,7 +27,7 @@ export function ThreeDCard({
 }: ThreeDCardProps) {
   return (
     <CardContainer className="inter-var">
-       <CardBody className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-6 border">
+       <CardBody className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-yellow-900/[9] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-6 border">
         <CardItem
           translateZ="50"
           className="text-2xl font-bold text-neutral-600 dark:text-white"
